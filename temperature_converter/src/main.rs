cbbd333d509818a26807681248881ec8243f40ce
Pavlo Kostrytsia <pahanela@gmail.com>
2025-05-26 14:36:32 -0700

    Implement f2c conversion

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn main() {
     println!("Temperature Units Converter");
-    println!("Please input the temperature in Fahrenheit:");
+    println!("Please input the temperature value in Fahrenheit:");
 
     let mut input: String = String::new();
 
@@ -12,5 +12,15 @@ fn main() {
 
     let input_f: f64 = input.trim().parse().expect("Please type a number!");
 
-    println!("You entered: {input_f}");
+    let output_c: f64 = convert_f2c(input_f);
+
+    println!("The corresponding temperature value in Celsius:\n{output_c}");
+}
+
+fn convert_f2c(temp: f64) -> f64 {
+    (temp - 32.0) * 5.0 / 9.0
+}
+
+fn convert_c2f(temp: f64) -> f64 {
+    temp * 9.0 / 5.0 + 32.0
 }
